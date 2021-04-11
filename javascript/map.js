ca3ca7d9448642af65533694f54870320bea709b
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-04-11 18:10:43 -0300

    alerta mobile | correcao html | divisao de pastas

--- javascript/map.js
+++ javascript/map.js
@@ -29,23 +29,4 @@ marker.bindPopup("<b>Ola viajante</b><br>Vocês está aqui.").openPopup();
     }
 }
 
-var cep = document.getElementById("cep")
-document.getElementById('button').onclick = ()=>{
-    fetch(`https://ws.apicep.com/cep/${cep.value}.json`).then
-    (Response => {return Response.json()}).then(json => 
-    {   document.getElementById('informe-distrito').innerText = json.district
-        document.getElementById('informa-cidade').innerText = json.address
-        document.getElementById('estado').innerText = json.city
-        }
-        );
-      window.localStorage.setItem("cep", cep.value)
-};
-
-if(localStorage.cep){
-    cep.value = localStorage.cep
-}
-
-
-
-
 pegarLocalizacao()
---
+++ javascript/mobile.js
@@ -0,0 +1,3 @@
+if(document.body.clientWidth < 570){
+   alert("Ligue seu GPS espere um pouco e atualize a pagina")
+}
